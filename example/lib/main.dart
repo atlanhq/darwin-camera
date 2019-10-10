edcacfab36e028ebf9e4a62fc1d74f1ed77492ab
askprateek <askpk123@gmail.com>
2019-10-10 21:08:27 +0530

    [ADDED] Required files

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -22,13 +22,13 @@ class _MyAppState extends State<MyApp> {
 
   // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
-    String platformVersion;
+    String platformVersion = "12";
     // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await DarwinCamera.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
+    // try {
+    //   platformVersion = await DarwinCamera.platformVersion;
+    // } on PlatformException {
+    //   platformVersion = 'Failed to get platform version.';
+    // }
 
     // If the widget was removed from the tree while the asynchronous platform
     // message was in flight, we want to discard the reply rather than calling
